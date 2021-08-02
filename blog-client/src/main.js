a785b120912ab2df19f7f256091aa4f632f8359d
Chee <cheevt@gmail.com>
2021-08-02 19:46:09 +0200

    listing posts

--- blog-client/src/main.js
+++ blog-client/src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue';
 import './assets/app.css';
 
 import router from './router';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8000';
 
 createApp(App).use(router).mount('#app')
--- blog-client/src/pages/Home.vue
+++ blog-client/src/pages/Home.vue
@@ -1,5 +1,29 @@
 <template>
-    <div>
-        Home
+    <div class="space-y-16">
+        <div v-for="post in posts" :key="post.uuid">
+            <a href="" class="block">
+                <h1 class="text-3xl sm:text-4xl leading-10 font-bold">{{ post.title }}</h1>
+                <p class="mt-6 text-gray-500">
+                    {{ post.teaser }}abc
+                </p>
+            </a>
+        </div>
     </div>
 </template>
+
+<script>
+import { onMounted } from 'vue';
+import usePosts from '../api/usePosts';
+
+export default {
+    setup() {
+        const { posts, fetchPosts } = usePosts();
+
+        onMounted(fetchPosts)
+
+        return {
+            posts
+        }
+    }
+}
+</script>
