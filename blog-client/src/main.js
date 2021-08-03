69176a2b2eee4fa58432663f56c145305fe57ae2
Chee <cheevt@gmail.com>
2021-08-03 19:35:27 +0200

    vuex store for authenticated

--- blog-client/src/main.js
+++ blog-client/src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue';
 import './assets/app.css';
 
 import router from './router';
+import store from './store';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:8000';
 axios.defaults.withCredentials = true;
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
