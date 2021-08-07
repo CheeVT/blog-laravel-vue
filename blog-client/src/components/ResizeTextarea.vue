4f969997f346d856dd5b0bf5065fd0366ef2acfb
Chee <cheevt@gmail.com>
2021-08-07 16:05:25 +0200

    renderless textarea height component

---
+++ blog-client/src/components/ResizeTextarea.vue
@@ -0,0 +1,28 @@
+<script>
+import { ref, onMounted } from 'vue';
+
+export default {
+    setup(props, context) {
+        const el = ref(null);
+
+        onMounted(() => {
+            resizeTraget(el.value);
+        })
+
+        const resizeTraget = (target) => {
+            target.style.height = 'auto';
+            target.style.height = `${target.scrollHeight}px`;
+        }
+        
+        const resize = (e) => {
+            console.log('resized', e);
+            resizeTraget(e.target);
+        }
+
+        return () => context.slots.default({
+            resize,
+            el
+        });
+    }
+}
+</script>
--- blog-client/src/pages/admin/Edit.vue
+++ blog-client/src/pages/admin/Edit.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
-        <textarea v-model="post.title" class="w-full text-center text-4xl lg:text-6xl leading-10 font-bold
-        tracking-tight text-gray-900 border-none focus:ring-0 resize-none p-0"></textarea>
+        <ResizeTextarea v-if="post.title">
+            <template v-slot:default="{ resize, el }">
+                <textarea :ref="el" v-model="post.title" v-on:input="resize" class="w-full text-center text-4xl lg:text-6xl leading-10 font-bold
+                tracking-tight text-gray-900 border-none focus:ring-0 resize-none p-0"></textarea>
+            </template>
+        </ResizeTextarea>
     </div>
 </template>
 
@@ -9,8 +13,12 @@
 import useAdminPosts from '../../api/useAdminPosts';
 import { onMounted, watch } from 'vue';
 import _ from 'lodash';
+import ResizeTextarea from '../../components/ResizeTextarea.vue';
 
 export default {
+    components: {
+        ResizeTextarea
+    },
     props: {
         slug: {
             required: true,
