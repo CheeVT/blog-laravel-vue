3539c62ab65435f2897984492def8600366f8e6a
Chee <cheevt@gmail.com>
2021-08-07 15:07:06 +0200

    fetching a post to edit - client

--- blog-client/src/pages/admin/Edit.vue
+++ blog-client/src/pages/admin/Edit.vue
@@ -1,5 +1,29 @@
 <template>
     <div>
-        Edit
+        {{ post }}
     </div>
 </template>
+
+<script>
+import useAdminPosts from '../../api/useAdminPosts';
+import { onMounted } from 'vue';
+
+export default {
+    props: {
+        slug: {
+            required: true,
+            type: String
+        }
+    },
+    setup(props) {
+        const { post, fetchPost } = useAdminPosts();
+
+        onMounted(fetchPost(props.slug));
+
+        return {
+            post
+        }
+
+    }
+}
+</script>
